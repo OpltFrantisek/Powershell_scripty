67937e505a49b228777f8179deead4910e98e708
foplt <opltfrantisek@gmail.com>
2019-07-01 08:22:42 +0200

    Pridan skripty pro práci se seriovym portem

---
+++ Com port/com_port.ps1
@@ -0,0 +1,6 @@
+﻿$port = new-Object System.IO.Ports.SerialPort COM6,115200,None,8,one
+$port.Open()
+$port.WriteLine( "at+csq" + "`r" )
+start-sleep -m 50
+$port.ReadExisting()
+$port.Close()
---
+++ Com port/odposlech_komunikace.ps1
@@ -0,0 +1,12 @@
+﻿#funkce pro prijem dat z com portu
+function read-com {
+    $port= new-Object System.IO.Ports.SerialPort COM3,9600,None,8,one
+    $port.Open()
+    do {
+        $line = $port.ReadLine()
+        Write-Host $line # Do stuff here
+    }
+    while ($port.IsOpen)
+}
+
+$COM = [System.IO.Ports.SerialPort]::getportnames() # zisakni vsech dostupnych portu
