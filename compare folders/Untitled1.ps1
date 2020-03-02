2838789fb1c7821514d11797332c2396a339fb58
OpltFrantisek <opltfrantisek@gmail.com>
2020-03-02 11:25:43 +0100

    add compare folders

---
+++ compare folders/Untitled1.ps1
@@ -0,0 +1,3 @@
+﻿$fso = Get-ChildItem -Recurse -path D:\Temp\a
+$fsoBU = Get-ChildItem -Recurse -path D:\Temp\b
+Compare-Object  $fso $fsoBU -Property LastWriteTime, FullName | Where-Object {$_.SideIndicator -eq "<="}
