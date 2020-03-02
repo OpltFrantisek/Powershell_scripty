b3158d65657d5bb409bb66619a35740641b3a4a3
OpltFrantisek <opltfrantisek@gmail.com>
2020-03-02 11:27:49 +0100

    Update Untitled1.ps1

--- compare folders/Untitled1.ps1
+++ compare folders/Untitled1.ps1
@@ -1,3 +1,3 @@
-﻿$fso = Get-ChildItem -Recurse -path D:\Temp\a
-$fsoBU = Get-ChildItem -Recurse -path D:\Temp\b
-Compare-Object  $fso $fsoBU -Property LastWriteTime, FullName | Where-Object {$_.SideIndicator -eq "<="}
+﻿$source = Get-ChildItem -Recurse -path D:\Temp\a
+$destination = Get-ChildItem -Recurse -path D:\Temp\b
+Compare-Object  $source $destination -Property LastWriteTime, FullName | Where-Object {$_.SideIndicator -eq "<="}
