8e2eb671ae1dfc65dcb53c8b8195dfc2d655517a
OpltFrantisek <opltfrantisek@gmail.com>
2020-03-02 11:34:21 +0100

    Update Untitled1.ps1

--- compare folders/Untitled1.ps1
+++ compare folders/Untitled1.ps1
@@ -1,6 +1,7 @@
 ﻿# Porovna soubory ze slozky a se slozkou b a zmeny zkopiruje do slozky c
 $source = Get-ChildItem -Recurse -path D:\Temp\a
 $destination = Get-ChildItem -Recurse -path D:\Temp\b
+$destinationCopy = "D:\Temp\c"
 Compare-Object  $source $destination -Property LastWriteTime, FullName | Where-Object {$_.SideIndicator -eq "<="} |  ForEach-Object {
-        Copy-Item "$($_.FullName)" -Destination "D:\Temp\c" -Force
+        Copy-Item "$($_.FullName)" -Destination $destinationCopy -Force
         }
